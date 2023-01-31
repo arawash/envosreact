565c7571021ccd4ddba59c2e1dbdc25ebe1af9a2
arawash <rawash.amr@gmail.com>
2023-01-31 20:29:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useState } from 'react';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
   return (
     <div className="App">
+<>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
