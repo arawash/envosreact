2bfa608e57a486e6873717cbcc088e470fbc7eb4
arawash <rawash.amr@gmail.com>
2023-01-31 20:47:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
+// Importing the Bootstrap CSS
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
