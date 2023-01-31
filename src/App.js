bfd6050aadef15b20001b9fec15bbfef19e87b84
arawash <rawash.amr@gmail.com>
2023-01-31 16:36:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://envos.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+EnvOS - Environmental Operating Systems
         </a>
       </header>
     </div>
